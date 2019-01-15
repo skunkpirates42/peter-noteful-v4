5984d59fb517bd361438052d23ad8ecbbde3ad3b
Peter Ramos <peterdramos02@gmail.com>
2019-01-15 14:36:39 -0500

    add JWT to auth route

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test',
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d';
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
